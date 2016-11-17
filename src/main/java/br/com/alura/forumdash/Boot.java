a34813cb1a6d4975e6e43b2ab2b4c6a6c4abd39d
asouza <alots.ssa@gmail.com>
2016-11-17 20:25:39 -0200

    consumindo e cacheando a alurona

--- src/main/java/br/com/alura/forumdash/Boot.java
+++ src/main/java/br/com/alura/forumdash/Boot.java
@@ -2,9 +2,10 @@ package br.com.alura.forumdash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class Boot {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/alura/forumdash/controller/ForumController.java
+++ src/main/java/br/com/alura/forumdash/controller/ForumController.java
@@ -1,20 +1,20 @@
 package br.com.alura.forumdash.controller;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @CrossOrigin
 @RestController
 public class ForumController {
 
 	@GetMapping(value="/sem-repostas", produces=MediaType.APPLICATION_JSON_VALUE)
+	@Cacheable(cacheNames="topics")
 	public String openTopics(){
-		return jsonPhill();
+		return new RestTemplate().getForObject("http://192.168.114.5:8080/d23hd728h7h8f70fh0f0837fh74387fh3478/forum/sem-respostas", String.class);
 	}
 
-	private String jsonPhill() {		
-		return "[\r\n  {\r\n    \"creationDate\": \"05/11/2016 11:00:00\",\r\n    \"days\": 10,\r\n    \"category\": \"Programa\u00E7\u00E3o\",\r\n    \"subcategory\": \"L\u00F3gica de Programa\u00E7\u00E3o\",\r\n    \"course\": \"Arduino\",\r\n    \"title\": \"N\u00E3o vai ter resposta!\",\r\n    \"link\": \"http://alura.com.br\"\r\n  },\r\n  {\r\n    \"creationDate\": \"05/11/2016 11:00:00\",\r\n    \"days\": 10,\r\n    \"category\": \"Programa\u00E7\u00E3o\",\r\n    \"subcategory\": \"L\u00F3gica de Programa\u00E7\u00E3o\",\r\n    \"course\": \"Arduino\",\r\n    \"title\": \"N\u00E3o vai ter resposta!\",\r\n    \"link\": \"http://alura.com.br\"\r\n  },\r\n  {\r\n    \"creationDate\": \"05/11/2016 11:00:00\",\r\n    \"days\": 10,\r\n    \"category\": \"Programa\u00E7\u00E3o\",\r\n    \"subcategory\": \"L\u00F3gica de Programa\u00E7\u00E3o\",\r\n    \"course\": \"Arduino\",\r\n    \"title\": \"N\u00E3o vai ter resposta!\",\r\n    \"link\": \"http://alura.com.br\"\r\n  }\r\n]";
-	}
 }

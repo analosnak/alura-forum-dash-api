78a02f6767bdc5814f32d6e045eacfafcec51d2c
Philippe Ehlert <philippe.ehlert@gmail.com>
2016-11-21 20:18:08 -0200

    colocando a url de producao e colocando os headers

--- src/main/java/br/com/alura/forumdash/controller/ForumController.java
+++ src/main/java/br/com/alura/forumdash/controller/ForumController.java
@@ -1,20 +1,23 @@
 package br.com.alura.forumdash.controller;
 
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @CrossOrigin
 @RestController
 public class ForumController {
 
-	@GetMapping(value="/sem-repostas", produces=MediaType.APPLICATION_JSON_VALUE)
-	@Cacheable(cacheNames="topics")
+	@GetMapping(value="/sem-respostas", produces=MediaType.APPLICATION_JSON_VALUE)
 	public String openTopics(){
-		return new RestTemplate().getForObject("http://192.168.114.5:8080/d23hd728h7h8f70fh0f0837fh74387fh3478/forum/sem-respostas", String.class);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("user-agent", "alura-dash");
+		
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> exchange = restTemplate.exchange("https://cursos.alura.com.br/d23hd728h7h8f70fh0f0837fh74387fh3478/forum/sem-respostas", HttpMethod.GET, entity, String.class);
+		return exchange.getBody();
 	}
 
 }
